ec7bb10448a6158d07b2e8dc944c5753c0a05e40
l1422586361 <1422586361@qq.com>
2020-04-07 00:15:59 +0800

    定义一个爬虫

--- README.md
+++ README.md
@@ -4,7 +4,12 @@ scrapy + mangoDB
 
 link：`https://realpython.com/web-scraping-with-scrapy-and-mongodb/`
 
-## 1.1. 项目初始化
+## 1.1. 本地环境
+
+`win + r`打开命令行，输入命令`activate py2.7`
+
+
+## 1.2. 项目初始化
 
 `python version:2.7.8`
 
@@ -12,6 +17,6 @@ link：`https://realpython.com/web-scraping-with-scrapy-and-mongodb/`
 
 `conda install pymongo`
 
-## 1.2. scrapy项目初始化
+## 1.3. scrapy项目初始化
 
 `scrapy startproject stack`
--- stack/stack/spiders/stack_spider.py
+++ stack/stack/spiders/stack_spider.py
@@ -2,9 +2,38 @@
 
 from scrapy import Spider
 
+# class StackSpider(Spider):
+#     name = 'stack'  # 定义爬虫名称
+#     allowed_domains = ["stackoverflow.com"] # 定义爬虫可抓取的允许域的基本URL
+#     start_urls = [
+#         "http://stackoverflow.com/questions?pagesize=50&sort=newest"    # 定义爬虫开始爬网的URL列表
+#     ]
+
+#     def parse(self,response):
+#         questions = Selector(response).xpath('//div[@class="summary"]/h3')
+
+#         for question in questions:
+#             itm = StackItem()
+#             item['title'] = question.xpath('a[@class = "question-hyperlink"]/text()').extract()[0]
+#             item['url'] = question.xpath('a[@class = "question-hyperlink"]/@href').extract()[0]
+#             yield item
+
 class StackSpider(Spider):
     name = 'stack'  # 定义爬虫名称
     allowed_domains = ["stackoverflow.com"] # 定义爬虫可抓取的允许域的基本URL
     start_urls = [
-        "http://stackoverflow.com/questions?pagesize=50&sort=newest"    # 定义爬虫开始爬网的URL列表
+        "https://www.bilibili.com/ranking?spm_id_from=333.851.b_7072696d61727950616765546162.3"    # 定义爬虫开始爬网的URL列表
     ]
+
+    def parse(self,response):
+        questions = Selector(response).xpath('//div[@class="info"]/h3')
+
+        for question in questions:
+            itm = StackItem()
+            item['title'] = question.xpath('a[@class = "title"]/text()').extract()[0]
+            item['url'] = question.xpath('a[@class = "title"]/@href').extract()[0]
+            yield item
+
+    # //*[@id="question-summary-61061485"]/div[2]
+
+
